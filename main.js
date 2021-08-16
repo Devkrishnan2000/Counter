8c6da7554bd7c9cafd1bfb28f06175a305fef1f9
Devkrishnan2000 <devanaswinikumar8@gmail.com>
2021-08-16 23:48:30 +0530

    counter program

---
+++ main.js
@@ -0,0 +1,18 @@
+
+    a =0
+    function add() 
+    {
+        a++
+        document.getElementById('numtxt').innerText = a
+        
+    }
+
+    function  sub() 
+    {
+         a--
+         document.getElementById('numtxt').innerText =a
+    }
+    function rst(params) {
+        a=0
+         document.getElementById('numtxt').innerText =a
+    }
